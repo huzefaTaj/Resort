f1dc77eb62e4e4b4756d007a05baeb5d9e6e62ad
unknown <huzefataj8@gmail.com>
2020-06-06 02:13:07 -0700

    first commit

---
+++ regis/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from . models import destination
+
+
+# Create your views here.
+#<-----login----------->
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalids')
+            return redirect('login')
+    else:
+        return  render( request,'login.html')
+#<------logout------->
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+#<--------register-------->
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'username takrn')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,email=email, password=password1, )
+                user.save();
+                return redirect('/')
+        else:
+            messages.info(request, 'password not matching')
+            return redirect('register')
+
+    else:
+
+        return render(request,'register.html')
+
+
+def home(request):
+    dests=destination.objects.all()
+    return render(request,'home.html',{'dests':dests})
+
+def user(request):
+   return  render(request,'about.html')
+
+
+
